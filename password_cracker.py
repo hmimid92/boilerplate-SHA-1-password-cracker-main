dfb43182e753955dc3d0a71b66b033c62a5a3fd1
HMIMID <ahmedhmimid177@gmail.com>
2025-04-24 23:29:12 +0100

    Initial commit

---
+++ password_cracker.py
@@ -0,0 +1,36 @@
+import hashlib
+
+def crack_sha1_hash(hash, use_salts = False):
+    returnFunction = ''
+    if use_salts:
+        with open('top-10000-passwords.txt') as fp:
+            for entry in fp:
+                ent = f'{entry.rstrip()}'
+                with open('known-salts.txt') as salt:
+                    for salti in salt:
+                        entNew = f'{salti.rstrip()}' + ent + f'{salti.rstrip()}'
+                        resultSalt = sign(entNew.encode('utf-8'))
+                        if resultSalt == hash:
+                           returnFunction = entry
+                           fp.close()
+                           salt.close()
+                           return returnFunction.replace('\n', '')
+    else:
+        with open('top-10000-passwords.txt') as fp:
+            for entry in fp:
+                ent = f'{entry.rstrip()}'
+                result = sign(ent.encode('utf-8'))
+                if result == hash:
+                    returnFunction = entry
+                    fp.close()
+                    return returnFunction.replace('\n', '')
+    return "PASSWORD NOT IN DATABASE"
+
+def sign(passWord):
+    hash_object = hashlib.sha1(passWord)
+    pbHash = hash_object.hexdigest()
+    # length = len(pbHash.decode("hex"))
+    # h = hashlib.sha1()
+    # h.update(passWord)
+    # h.digest()
+    return pbHash
